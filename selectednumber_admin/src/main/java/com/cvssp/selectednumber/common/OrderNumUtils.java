f90fa8a8d7c12d7c46f638748a7bebe9db3f15d3
wanggq1109 <33042974@qq.com>
2017-06-28 16:11:58 +0800

    创建订单工具类

---
+++ selectednumber_admin/src/main/java/com/cvssp/selectednumber/common/OrderNumUtils.java
@@ -0,0 +1,57 @@
+package com.cvssp.selectednumber.common;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by wgq on 2017/6/28.
+ */
+public class OrderNumUtils {
+
+
+    /**
+     * 锁对象，可以为任意对象
+     */
+    private static Object lockObj = "lockerOrder";
+    /**
+     * 订单号生成计数器
+     */
+    private static long orderNumCount = 0L;
+    /**
+     * 每毫秒生成订单号数量最大值
+     */
+    private static int maxPerMSECSize = 1000;
+
+
+    /**
+     * 生成非重复订单号，理论上限1毫秒1000个，可扩展
+     *
+     *
+     */
+    public static String makeOrderNum() {
+
+        // 最终生成的订单号
+        String finOrderNum = "";
+
+        try {
+            synchronized (lockObj) {
+                // 取系统当前时间作为订单号变量前半部分，精确到毫秒
+                long nowLong = Long.parseLong(new SimpleDateFormat("yyyyMMddHHmmssSSS").format(new Date()));
+                // 计数器到最大值归零，可扩展更大，目前1毫秒处理峰值1000个，1秒100万
+                if (orderNumCount >= maxPerMSECSize) {
+                    orderNumCount = 0L;
+                }
+                //组装订单号
+                String countStr = maxPerMSECSize + orderNumCount + "";
+                finOrderNum = nowLong + countStr.substring(1);
+                orderNumCount++;
+                //System.out.println(finOrderNum + "--" + Thread.currentThread().getName() + "::" + tname );
+                // Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return finOrderNum;
+    }
+}
